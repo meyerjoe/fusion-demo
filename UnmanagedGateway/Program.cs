0ec42fe2fa901d458d0dac67b81f10f87f6ab93a
meyerjoe <meyerjoe@gmx.com>
2023-07-26 23:03:48 +0200

    feat(UnmanagedGateway): added instrumentation

--- UnmanagedGateway/Program.cs
+++ UnmanagedGateway/Program.cs
@@ -1,12 +1,42 @@
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHttpClient("Fusion");
+builder.Services
+    .AddHttpClient("Fusion")
+    .AddHeaderPropagation();
 
 builder.Services
     .AddFusionGatewayServer()
-    .ConfigureFromFile("./gateway.fgp");
+    .ConfigureFromFile("./gateway.fgp")
+    .CoreBuilder
+    .AddInstrumentation(o => o.RenameRootActivity = true);
+
+builder.Services
+    .AddOpenTelemetry()
+    .ConfigureResource(b => b.AddService("Shop-Gateway", "Demo", Env.Version))
+    .WithTracing(
+        b =>
+        {
+            b.AddHttpClientInstrumentation();
+            b.AddAspNetCoreInstrumentation();
+            b.AddHotChocolateInstrumentation();
+            b.AddOtlpExporter();
+        })
+    .WithMetrics(
+        b =>
+        {
+            b.AddHttpClientInstrumentation();
+            b.AddAspNetCoreInstrumentation();
+            b.AddOtlpExporter();
+        });
 
 var app = builder.Build();
+
+app.UseHeaderPropagation();
 
 app.MapGraphQL();
 
