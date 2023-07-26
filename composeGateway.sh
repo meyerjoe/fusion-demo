0407c943e86ca917f621c9f4f7719e2fb63fed88
meyerjoe <meyerjoe@gmx.com>
2023-07-26 09:56:37 +0200

    feat: added notification subgraph to unmanaged gateway

--- composeGateway.sh
+++ composeGateway.sh
@@ -2,3 +2,4 @@ dotnet fusion compose -p ./UnmanagedGateway/gateway -s ./Subgraphs/Accounts
 dotnet fusion compose -p ./UnmanagedGateway/gateway -s ./Subgraphs/Products
 dotnet fusion compose -p ./UnmanagedGateway/gateway -s ./Subgraphs/Reviews
 dotnet fusion compose -p ./UnmanagedGateway/gateway -s ./Subgraphs/Shipping
+dotnet fusion compose -p ./UnmanagedGateway/gateway -s ./Subgraphs/Notifications
--- packSubgraphs.sh
+++ packSubgraphs.sh
@@ -2,3 +2,4 @@ dotnet fusion subgraph pack -w ./Subgraphs/Accounts
 dotnet fusion subgraph pack -w ./Subgraphs/Products
 dotnet fusion subgraph pack -w ./Subgraphs/Reviews
 dotnet fusion subgraph pack -w ./Subgraphs/Shipping
+dotnet fusion subgraph pack -w ./Subgraphs/Notifications
